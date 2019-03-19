0c6bfc87bf8ea216c82247f4694ba20b762a0b4a
Tim Woelfle <tim.woelfle@web.de>
2019-03-19 19:27:43 +0000

    Renamed package to CH3, added compare_sex(), linear_mixed_models() and documentation

--- R/workflow.R
+++ R/workflow.R
@@ -12,10 +12,10 @@ eira = preprocess("hm450", "../idat", return_intensities=F, verbose=TRUE)
 
 # Remove samples with too many missing values (>10%) or failed bisulfite conversion
 # Remove CpG sites with too many missing values (>10%)
-eira_red = remove_unreliable_samples_probes(eira, threshold_NA_sample = 0.1, threshold_NA_cpg = 0.1, bc_threshold = 1, plot=T)
+eira = remove_unreliable_samples_probes(eira, threshold_NA_sample = 0.1, threshold_NA_cpg = 0.1, bc_threshold = 1, plot=T)
 
 # Infer sex
-sex = infer_sex(eira, kmeans=T, plot=T)
+inferred_sex = infer_sex(eira, kmeans=T, plot=T)
 
 # Call SNPs
 snps = call_snps(eira, plot=T)
@@ -26,4 +26,27 @@ identify_replicates(eira)
 
 # Estimate leukocyte-subtype proportions
 estimate_leukocytes(eira)
+
+
+
+### Analysis
+
+library(omics)
+
+# Read covariates and table containing ids
+covars = readRDS("../Covariates.rds")
+sample_sheet = readRDS("../Sample_sheet.rds")
+
+# Set rownames of covariates to match id-format of dnam-dataframes
+rownames(covars) = sample_sheet$sample.id
+
+# Compare inferred sex with prior knowledge
+supplied_sex = covars$gender
+names(supplied_sex) = rownames(covars)
+compare_sex(eira, supplied_sex)
+
+# Linear mixed models
+outcome = covars$disease_status == "rheumatoid arthritis"
+names(outcome) = rownames(covars)
+models = linear_mixed_models(eira, outcome, confounders=covars[,3:5], plot=T)
 
