7aaa9187d04d97324fbc3c3ad97f61338d1c4da9
baerbelblume <metzlerbar@gmail.com>
2019-03-20 14:06:08 +0000

    update workflow

--- Python_package/workflow.py
+++ Python_package/workflow.py
@@ -8,23 +8,20 @@ from illuminaio import list_idat
 
 ##load data for testing
 # define paths
-csv_file= 'DNAm/python/dnam/R05C01_beads.csv'
-data = load_data(csv_file)
+data_file= 'DNAm/python/dnam/R05C01_beads.csv'
+data = load_data(data_file)
+# shape (622399, 7)
+data = data[1:10000]
 
 
 #read manifests
-probes_file = 'DNAm/python/illumina_manifests/hm450_probes.rds'
+#probes_file = 'DNAm/python/illumina_manifests/hm450_probes.rds'
 #controls_file = 'DNAm/python/illumina_manifests/hm450_controls.rds'
+
 #quick fix -csv file
-#controls_file = 
+probes_file = 'DNAm/python/testing/probes.csv'
+controls_file = 'DNAm/python/testing/control_beads.csv'
 
 probes, controls = read_manifests(probes_file, controls_file)
 
-
-##Preparation of the outputs
-idat_files_folder = 'idat/'
-
-preperation_outputs(probes, idat_files_folder)
-
-##Create intensities
-intensities, controls = create_intensities(data, probes, controls, idat_files, arg_beads=3, )
+##Preprocessing
